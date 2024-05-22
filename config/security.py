62f98db9a2b17a45e35e65b3135c43115265aa6d
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2024-05-22 16:17:28 -0300

    refact: hosts

--- config/security.py
+++ config/security.py
@@ -2,13 +2,13 @@ from decouple import config as env
 
 SECRET_KEY = env('SECRET_KEY')
 
-HOSTS = [
-    '127.0.0.1:8000', 'localhost:8000', 'cbt2.icmc.usp.br:8000',
-]
+PORT = env('PORT', default=8000, cast=int)
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 'localhost', 'cbt2.icmc.usp.br',
 ]
+
+HOSTS = [f'{host}:{PORT}' for host in ALLOWED_HOSTS]
 
 CSRF_TRUSTED_ORIGINS = [f'http://{host}' for host in HOSTS]
 
