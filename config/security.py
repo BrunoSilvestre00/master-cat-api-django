a4766ecdc22b943e443c27dbfc8225caa302ec8b
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2024-05-22 15:18:03 -0300

    feat: update hosts

--- config/security.py
+++ config/security.py
@@ -3,11 +3,11 @@ from decouple import config as env
 SECRET_KEY = env('SECRET_KEY')
 
 HOSTS = [
-    '127.0.0.1:8000', 'localhost:8000',
+    '127.0.0.1:8000', 'localhost:8000', 'cbt2.icmc.usp.br:8000',
 ]
 
 ALLOWED_HOSTS = [
-    '127.0.0.1', 'localhost',
+    '127.0.0.1', 'localhost', 'cbt2.icmc.usp.br',
 ]
 
 CSRF_TRUSTED_ORIGINS = [f'http://{host}' for host in HOSTS]
